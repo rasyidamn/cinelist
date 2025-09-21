51024ed5c420eef58e03ef453cdba264ac5b7782
rasyidamn <132162495+rasyidamn@users.noreply.github.com>
2025-09-21 08:59:07 +0700

    fixing unused variable

--- frontend/src/components/CarouserContainer.tsx
+++ frontend/src/components/CarouserContainer.tsx
@@ -1,5 +1,4 @@
 import { useRef } from "react";
-import { cn } from "../lib/utils";
 import { ArrowLeft, ArrowRight } from "lucide-react";
 import type { Result } from "../api/fetchMovies";
 
@@ -31,8 +30,7 @@ export default function CarouserContainer({
 		}
 	};
 
-	const Url =
-		"https://plus.unsplash.com/premium_photo-1661675440353-6a6019c95bc7?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+	
 	return (
 		<div>
 			<h2 className="text-2xl">{category}</h2>
