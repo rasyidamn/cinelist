c44c0763e6ae44527cbadc0d746dbb906ac242a5
rasyidamn <132162495+rasyidamn@users.noreply.github.com>
2025-10-02 20:36:41 +0700

    menambah detail lmovie

--- frontend/src/components/TrailerModal.tsx
+++ frontend/src/components/TrailerModal.tsx
@@ -1,15 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
+// import { useQuery } from "@tanstack/react-query";
 import { PlayIcon } from "lucide-react";
-import { fetchMovieTrailers } from "../api/fetchMovies";
+// import { fetchMovieTrailers } from "../api/fetchMovies";
 
 export default function TrailerModal({movieId}:{movieId:string}) {
-   const queryMovieTrailer = useQuery({
-      queryKey: ["movieTrailers", movieId],
-      queryFn: () => fetchMovieTrailers(movieId)
-   })
+   // const queryMovieTrailer = useQuery({
+   //    queryKey: ["movieTrailers", movieId],
+   //    queryFn: () => fetchMovieTrailers(movieId)
+   // })
 
-   const trailerUrl = queryMovieTrailer.data?.data.results[0]
-   
+   // const trailerUrl = queryMovieTrailer.data?.data.results[0]
+
 
 	return (
 		<div>
@@ -28,16 +28,7 @@ export default function TrailerModal({movieId}:{movieId:string}) {
 			</button>
 			<dialog id="my_modal_2" className="modal">
 				<div className="relative w-full aspect-video">
-					{" "}
-					{/* Gunakan aspect-video untuk rasio aspek 16:9 */}
-					<iframe
-						className="absolute top-0 left-0 w-full h-full rounded-lg"
-						// src={embedUrl}
-						// title={title} // Penting untuk aksesibilitas
-						// frameBorder="0"
-						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-						allowFullScreen
-					></iframe>
+					
 				</div>
 			</dialog>
 		</div>
