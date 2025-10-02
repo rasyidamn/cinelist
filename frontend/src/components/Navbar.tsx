9148bd4b62d514d32e2e6d5549c9954217106e2a
rasyidamn <132162495+rasyidamn@users.noreply.github.com>
2025-10-03 06:36:39 +0700

    menambah trailer di movie details

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -8,7 +8,9 @@ export default function Navbar() {
 	return (
 		<nav className="flex w-full px-6 sm:h-16 z-50 justify-between sm:pl-4 sm:pr-10 h-14 shadow-xl sticky top-0 backdrop-blur-xl">
 			<div className="flex  p-2 justify-start items-center w-32 sm:w-40 overflow-hidden">
-				<img src={logo} alt="" className="relative top-1" />
+				<NavLink to="/">
+					<img src={logo} alt="" className="relative top-1" />
+				</NavLink>
 			</div>
 
 			<div className="hidden sm:flex justify-center items-center sm:gap-6 lg:gap-10">
@@ -16,10 +18,12 @@ export default function Navbar() {
 					<NavLink
 						to={item.path}
 						key={item.title}
-						className={({isActive}) => cn(
-							"px-4 py-1 flex justify-center items-center gap-2 rounded-full hover:bg-primary/10 hover:text-secondary hover:translate-y-0.5 transition duration-300 ease-in-out",
-                     isActive ? "text-secondary bg-primary/20" : "",
-						)}
+						className={({ isActive }) =>
+							cn(
+								"px-4 py-1 flex justify-center items-center gap-2 rounded-full hover:bg-primary/10 hover:text-secondary hover:translate-y-0.5 transition duration-300 ease-in-out",
+								isActive ? "text-secondary bg-primary/20" : ""
+							)
+						}
 					>
 						{item.icon}
 						{item.title}
--- frontend/src/components/TrailerModal.tsx
+++ frontend/src/components/TrailerModal.tsx
@@ -1,36 +1,72 @@
-// // import { useQuery } from "@tanstack/react-query";
-// import { PlayIcon } from "lucide-react";
-// // import { fetchMovieTrailers } from "../api/fetchMovies";
+import { useQuery } from "@tanstack/react-query";
+import { PlayIcon } from "lucide-react";
+import { fetchMovieTrailers } from "../api/fetchMovies";
+import { useEffect, useRef, useState } from "react";
 
-// export default function TrailerModal({movieId}:{movieId:string}) {
-//    // const queryMovieTrailer = useQuery({
-//    //    queryKey: ["movieTrailers", movieId],
-//    //    queryFn: () => fetchMovieTrailers(movieId)
-//    // })
+export default function TrailerModal({ movieId }: { movieId: string }) {
+	const queryMovieTrailer = useQuery({
+		queryKey: ["movieTrailers", movieId],
+		queryFn: () => fetchMovieTrailers(movieId),
+	});
 
-//    // const trailerUrl = queryMovieTrailer.data?.data.results[0]
+	const trailerUrl = queryMovieTrailer.data?.data.results[0];
+	const embedUrl = `https://www.youtube.com/embed/${trailerUrl?.key}`;
 
+   const [activeUrl, setActiveUrl] = useState<string>("")
+   const modalRef = useRef<HTMLDialogElement>(null)
 
-// 	return (
-// 		<div>
-// 			<button
-// 				className="btn"
-// 				onClick={() => {
-// 					const modal = document.getElementById(
-// 						"my_modal_2"
-// 					) as HTMLDialogElement | null;
-// 					if (modal) {
-// 						modal.showModal();
-// 					}
-// 				}}
-// 			>
-// 				<PlayIcon size={20} /> Watch Trailer
-// 			</button>
-// 			<dialog id="my_modal_2" className="modal">
-// 				<div className="relative w-full aspect-video">
-					
-// 				</div>
-// 			</dialog>
-// 		</div>
-// 	);
-// }
+   const openModal = () => {
+      if(trailerUrl?.key) {
+
+         setActiveUrl(embedUrl)
+         modalRef.current?.showModal()
+      }
+   }
+
+   const closeModal = () => {
+      setActiveUrl("")
+   }
+
+   useEffect(() => {
+      if(modalRef.current) {
+         modalRef.current.addEventListener('close', closeModal)
+      }
+      return () => {
+         modalRef.current?.removeEventListener('close', closeModal)
+      }
+   }, [modalRef])
+
+	return (
+		<div>
+			<button
+				className="btn"
+				onClick={openModal}
+			>
+				<PlayIcon size={20} /> Watch Trailer
+			</button>
+			<dialog id="my_modal_2" className="modal" ref={modalRef}>
+				<div className="modal-box w-2/3 max-w-5xl pt-10">
+					<form method="dialog">
+						{/* if there is a button in form, it will close the modal */}
+						<button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+							✕
+						</button>
+					</form>
+					<div className="relative w-full aspect-video">
+						<iframe
+							className="absolute top-0 left-0 w-full h-full rounded-lg"
+							src={activeUrl}
+							// title={title} // Penting untuk aksesibilitas
+							frameBorder="0"
+							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+							allowFullScreen
+						></iframe>
+					</div>
+				</div>
+				<form method="dialog" className="modal-backdrop ">
+					<button>close</button>
+				</form>
+			</dialog>
+		</div>
+	);
+}
